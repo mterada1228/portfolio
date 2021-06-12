86ea6ace51fda764444b45ded99a670907e85d1b
mterada1228 <mterada1228@gmail.com>
2021-06-12 16:42:40 +0900

    Home コンポーネントを追加して、Hello World を表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,9 @@
-import React from "react";
-import { Button } from "@material-ui/core";
-import logo from "./logo.svg";
+import { FC } from "react";
+import Home from "./components/pages/Home";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Button color="primary">Hello World</Button>
-    </div>
-  );
-}
+const App: FC = () => {
+  return <Home />;
+};
 
 export default App;
---
+++ src/components/pages/Home.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const Home: FC = () => {
+  return (
+    <>
+      <p>Hello World</p>
+    </>
+  );
+};
+
+export default Home;
