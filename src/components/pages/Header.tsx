493a0013cde677cffd09264b96778b46877a5cc5
mterada1228 <mterada1228@gmail.com>
2021-09-12 20:18:00 +0900

    Home ページを作成

--- src/components/pages/Header.tsx
+++ src/components/pages/Header.tsx
@@ -1,15 +1,12 @@
 import { FC } from "react";
 import { AppBar, Toolbar, Typography } from "@material-ui/core";
-import { makeStyles } from "@material-ui/styles";
-
-const useStyle = makeStyles({});
 
 const Header: FC = () => {
   return (
     <>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6">News</Typography>
+          <Typography variant="h6">Home</Typography>
         </Toolbar>
       </AppBar>
     </>
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,7 +1,41 @@
 import { FC } from "react";
+import { Container, makeStyles, Typography } from "@material-ui/core";
+import topImage from "../../images/monkey.png";
+
+const useStyle = makeStyles((theme) => ({
+  root: {
+    textAlign: "center",
+  },
+  h3: {
+    marginBottom: theme.spacing(4),
+    marginTop: theme.spacing(4),
+  },
+  h2: {
+    marginBottom: theme.spacing(4),
+    marginTop: theme.spacing(4),
+  },
+  topImage: {
+    width: "300px",
+    height: "300px",
+  },
+}));
 
 const Home: FC = () => {
-  return <>Hello Home page</>;
+  const classes = useStyle();
+
+  return (
+    <>
+      <Container className={classes.root}>
+        <Typography variant="h3" className={classes.h3}>
+          Masaru Terada
+        </Typography>
+        <Typography variant="h2" className={classes.h2}>
+          Web developer's base
+        </Typography>
+        <img src={topImage} alt="monkey" className={classes.topImage} />
+      </Container>
+    </>
+  );
 };
 
 export default Home;
