41449127280172bf6acffc0e264da68de5aea078
mterada1228 <mterada1228@gmail.com>
2021-09-12 15:35:14 +0900

    Home ページを初期化

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,51 +1,7 @@
-import * as React from "react";
-import { Theme } from "@material-ui/core/styles";
-import Typography from "@material-ui/core/Typography";
-import { withStyles, WithStyles } from "@material-ui/styles";
-import Container from "@material-ui/core/Container";
-import { fade } from "@material-ui/core/styles/colorManipulator";
-import backgroundImage from "../../images/gihyojp_logo.png";
+import { FC } from "react";
 
-const styles = (theme: Theme) => ({
-  container: {
-    alignItems: "center",
-    backgroundColor: fade("#DED780", 0.5),
-  },
-  title: {
-    color: "#FFFFFF",
-    paddingTop: 30,
-  },
-  image: {},
-});
+const Home: FC = () => {
+  return <>Hello Home page</>;
+};
 
-function Home(props: WithStyles<typeof styles>) {
-  const { classes } = props;
-
-  return (
-    <div>
-      <Container className={classes.container}>
-        <Typography
-          variant="h2"
-          color="inherit"
-          noWrap
-          align="center"
-          className={classes.title}
-        >
-          Terada Masaru
-        </Typography>
-        <Typography
-          variant="h2"
-          color="inherit"
-          noWrap
-          align="center"
-          className={classes.title}
-        >
-          Portfolio site
-        </Typography>
-        <img src={backgroundImage} alt="monkey" className={classes.image} />
-      </Container>
-    </div>
-  );
-}
-
-export default withStyles(styles)(Home);
+export default Home;
